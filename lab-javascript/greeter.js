fe4d930993f63473dbaedf5482bf7343f897309e
rachel-tilt <rachel@wearetilt.com>
2015-11-05 12:57:12 +0000

    first changes

--- lab-javascript/greeter.js
+++ lab-javascript/greeter.js
@@ -1,5 +1,47 @@
+var Student = (function () {
+    function Student(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Student;
+})();
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstname + " " + person.lastname;
 }
-var user = "Martin Beeby";
-document.body.innerHTML = greeter(user);
+var user = new Student("Martin", "L", "Beeby");
+var user1 = new Student("Martin", "Mavrick", "Kearn");
+var user2 = new Student("Mike", "The Hit Man", "Taulty");
+// Create 3 new variables and populate them from an array.
+// Now we have 3 new variables called beeby, kearn , taulty
+var _a = printStudents(user, user1, user2), beeby = _a[0], kearn = _a[1], taulty = _a[2];
+// We can now send the beeby object to be printed again.
+printStudents(beeby);
+function printStudents() {
+    var listOfStudents = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        listOfStudents[_i - 0] = arguments[_i];
+    }
+    for (var _a = 0; _a < listOfStudents.length; _a++) {
+        var v = listOfStudents[_a];
+        document.body.innerHTML += greeter(v);
+    }
+    return listOfStudents;
+}
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        var _this = this;
+        // Notice: the line below is now a lambda, allowing us to capture 'this' earlier
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    }
+};
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);
--- lab-javascript/greeter.ts
+++ lab-javascript/greeter.ts
@@ -1,9 +1,52 @@
-function greeter(person) {
-    return "Hello, " + person;
+class Student {
+    fullname : string;
+    constructor(public firstname, public middleinitial, public lastname) {
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
 }
 
-var user = "Martin Beeby";
+interface Person {
+    firstname: string;
+    lastname: string;
+}
 
-greeter(4323);
+function greeter(person : Person) {
+        return `Hello, ${person.firstname} ${person.lastname}`;
+}
 
-document.body.innerHTML = greeter(user);
+var user = new Student("Martin", "L", "Beeby");
+var user1 = new Student("Martin", "Mavrick", "Kearn");
+var user2 = new Student("Mike", "The Hit Man", "Taulty");
+
+// Create 3 new variables and populate them from an array.
+// Now we have 3 new variables called beeby, kearn , taulty
+var [beeby, kearn, taulty] = printStudents(user,user1, user2)
+
+// We can now send the beeby object to be printed again.
+printStudents(beeby)
+
+function printStudents(...listOfStudents: Student[]) {
+    for (var v of listOfStudents) {
+            document.body.innerHTML += greeter(v);
+    }
+    return listOfStudents;
+}
+
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function() {
+        // Notice: the line below is now a lambda, allowing us to capture 'this' earlier
+        return () => {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);
